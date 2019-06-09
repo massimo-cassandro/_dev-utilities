23baa2100b3f12cf2db8ae1d3bd2a1942e42a8d9
Massimo Cassandro <massimo.cassandro@blu-net.it>
2019-06-09 11:48:03 +0200

    no message

---
+++ script/dev app launcher mini.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+open -a "Coda 2"
+open -a "CodeKit"
+open -a "MAMP PRO"
+open -a "Dash"
+
+open -a "Evernote" 
+open -a "Visual Studio Code"
+
+
+open -a "Firefox"
+open -a "Firefox Developer Edition"
+
+#osascript -e 'quit app "Terminal"'
